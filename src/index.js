6b12c75333c15f94c9981abea49eb192c497f085
raihanputro <raihanputromaulana477@gmail.com>
2022-08-29 05:32:48 +0700

    Adding CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ContactApp from './contactApp';
+import ContactApp from './components/contactApp';
+import './style/style.css'
 
 
 
